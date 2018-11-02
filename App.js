6456b6f4f67586c0a584ba0824034efcd2cead2e
Jiri Koutny <jirikoutny@Jiri-MacBook-Pro.local>
2018-11-02 15:47:28 +0100

    Tracking message sample

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import Analytics from 'appcenter-analytics';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,6 +19,9 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentDidMount() {
+    Analytics.trackEvent('App started');
+  }
   render() {
     return (
       <View style={styles.container}>
--- index.js
+++ index.js
@@ -3,5 +3,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import Analytics from 'appcenter-analytics';
+
+Analytics.trackEvent('App starting');
 
 AppRegistry.registerComponent(appName, () => App);
